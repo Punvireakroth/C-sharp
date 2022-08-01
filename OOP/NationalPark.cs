8cedcba9863fcb5191616ab0642c1744b83c93a2
Punvireakroth <vireakrothpun@gmail.com>
2022-08-01 12:52:21 +0700

    initial commit

---
+++ OOP/NationalPark.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BasicClasses
+{
+  class Forest
+  {
+    // field
+    public string name;
+    public string biome;
+    public int trees;
+    public int age;
+  }
+
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace BasicClasses
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Forest f = new Forest();
+      f.name = "Kirirom";
+      f.biome = "vireakroth";
+      f.trees = 1200;
+      f.age = 70;
+    }
+  }
+}
