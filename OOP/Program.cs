d0e81b33ff76e5b966961f49e184b419c12e7f8d
Punvireakroth <vireakrothpun@gmail.com>
2022-08-01 12:56:12 +0700

    access change and print data

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,11 +6,18 @@ namespace BasicClasses
   {
     static void Main(string[] args)
     {
+        // Create a object and acess then change the field data
       Forest f = new Forest();
       f.name = "Kirirom";
       f.biome = "vireakroth";
       f.trees = 1200;
       f.age = 70;
+    //   After change the field data I print those data to the console
+      Console.WriteLine(f.name);
+      Console.WriteLine(f.biome);
+      Console.WriteLine(f.trees);
+      Console.WriteLine(f.age);
+
     }
   }
 }
