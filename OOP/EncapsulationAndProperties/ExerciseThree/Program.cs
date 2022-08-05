553518cf38fe0090d0b3e29fb0f79e092369b059
Punvireakroth <vireakrothpun@gmail.com>
2022-08-05 10:37:33 +0700

    practice exercise 3

---
+++ OOP/EncapsulationAndProperties/ExerciseThree/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseThree
+{
+    class Circle
+    {
+        private float fltRadius;
+        private string strColor;
+
+        public float Radius
+        {
+            get { return fltRadius; }
+            set 
+            { 
+                if(value > 0)
+                {
+                    fltRadius = value;
+                } 
+            }
+        }
+
+        public string Color
+        {
+            get { return strColor; }
+            set { strColor = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle cObj = new Circle();
+            Console.WriteLine("Enter radius: ");
+            cObj.Radius = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Color: ");
+            cObj.Color = Console.ReadLine();
+
+            Console.WriteLine("Radius: {0} Color {1}", cObj.Radius, cObj.Color);
+
+        }
+    }
+}
