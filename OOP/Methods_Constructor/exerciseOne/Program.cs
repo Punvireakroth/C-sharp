26e3399990bf01eaecee747f2d69538dd1989d21
Punvireakroth <vireakrothpun@gmail.com>
2022-08-05 16:17:10 +0700

    initial commit

---
+++ OOP/Methods_Constructor/exerciseOne/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerciseOne
+{
+    class Rectangle
+    {
+        private float fltLength;
+        private float fltBreadth;
+
+        public float Area
+        {
+            get { return fltLength * fltBreadth; }
+        }
+
+
+        public Rectangle(float a, float b)
+        {
+            this.fltLength = a;
+            this.fltBreadth = b;
+        }
+
+        public string GetSize()
+        {
+            return "The length and breadth of the rectangle is" + fltLength.ToString() + "cm " + fltBreadth.ToString();
+        }
+    }
+
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1;
+            Rectangle r2;
+
+
+            Console.WriteLine("Length 1 : ");
+            float length = float.Parse(Console.ReadLine());
+            Console.WriteLine("Bredth 1 : ");
+            float breadth = float.Parse(Console.ReadLine());
+
+            r1 = new Rectangle(length, breadth);
+            Console.WriteLine(r1.GetSize());
+
+            Console.WriteLine("Length 2 : ");
+            float length2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Bredth 2 : ");
+            float breadth2 = float.Parse(Console.ReadLine());
+
+            r2 = new Rectangle(length2, breadth2);
+            Console.WriteLine(r2.GetSize());
+
+        }
+    }
+}
---
+++ OOP/polymorphism_methodOverload/polymorphism_methodOverload/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism_methodOverload
+{
+    class MedicalRecords
+    {
+        private string strPatientName;
+        private float fltMedicalFees;
+
+        public MedicalRecords(string a, float b)
+        {
+            this.strPatientName = a;
+            this.fltMedicalFees = b;
+        }
+
+        public float CalMedicalFees()
+        {
+            return fltMedicalFees;
+        }
+
+        public float CalMedicalFees(string test)
+        {
+            if(test == "Hepatities")
+            {
+                fltMedicalFees += 17.7f;
+            }else if(test == "Cholesterol")
+            {
+                fltMedicalFees += 25.00f;
+            }else if(test == "Diabetes")
+            {
+                fltMedicalFees += 20.00f;
+            }
+            return fltMedicalFees;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter patient's name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Medical fees: ");
+            float price = float.Parse(Console.ReadLine());
+            MedicalRecords mObj = new MedicalRecords(name, price);
+
+            Console.WriteLine("Medical fees = {0}", mObj.CalMedicalFees().ToString());
+
+            Console.WriteLine("Enter the test: ");
+            string testType = Console.ReadLine();
+            
+            Console.WriteLine("Medical record afer test {0}", mObj.CalMedicalFees(testType).ToString());
+        }
+    }
+}
